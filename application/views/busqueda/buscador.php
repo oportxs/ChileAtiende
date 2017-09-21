374f3109dd53162196504d299bb3ff40b0df53b4
Juan Carlos Jiménez <jcjj.system@gmail.com>
2017-09-21 12:55:51 -0300

    fix para barra búsqueda responsive

--- application/views/busqueda/buscador.php
+++ application/views/busqueda/buscador.php
@@ -10,10 +10,13 @@
 			$e_value = 0;
 	}
 ?>
-<div class="cont-busqueda">
+<div class="cont-busqueda span12">
     <form action="<?= site_url('buscar/fichas') ?>" method="get" id="main_search">
-        <input title="Escribe aquí lo que buscas" accesskey="b" id="main_search_input" class="main_search_input span12 <?php echo (!$this->config->item("lite_mode"))?'active_search':''; ?>" autocomplete="off" name="buscar" placeholder="Escribe aquí lo que buscas" type="text" <?php echo (isset($buscar)) ? "value='" . htmlspecialchars($buscar,ENT_QUOTES) . "'" : "" ?> />
-        <button type="submit" accesskey="s" class="searchbtn"><span class="fa fa-search" aria-hidden="true"></span> Buscar</button>
+        <input title="Escribe aquí lo que buscas" accesskey="b" id="main_search_input" class="main_search_input <?php echo (!$this->config->item("lite_mode"))?'active_search':''; ?>" autocomplete="off" name="buscar" placeholder="Escribe aquí lo que buscas" type="text" <?php echo (isset($buscar)) ? "value='" . htmlspecialchars($buscar,ENT_QUOTES) . "'" : "" ?> />
+        <button type="submit" accesskey="s" class="searchbtn">
+        	<span class="fa fa-search" aria-hidden="true"></span> 
+        	<span class="etiqueta"> Buscar</span>
+        </button>
         <input type="hidden" name="e" value="<?=$e_value?>">
     </form>
 </div>
